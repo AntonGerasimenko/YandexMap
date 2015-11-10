4e905b5c2280905c1dd1104b45858654dce8d03c
TrubkaU <dubrava123@gmail.com>
2015-11-10 18:51:47 +0300

    add another Views

---
+++ app/src/main/java/by/minsk/gerasimenko/anton/yandexmap/views/CardOut.java
@@ -0,0 +1,24 @@
+package by.minsk.gerasimenko.anton.yandexmap.views;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gerasimenko on 10.11.2015.
+ */
+public class CardOut  extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+        View view  = inflater.inflate(,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/by/minsk/gerasimenko/anton/yandexmap/views/TaskList.java
@@ -0,0 +1,23 @@
+package by.minsk.gerasimenko.anton.yandexmap.views;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+
+/**
+ * Created by gerasimenko on 10.11.2015.
+ */
+public class TaskList extends ListFragment {
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+
+        /*ListAdapter
+
+        setListAdapter();*/
+
+        super.onActivityCreated(savedInstanceState);
+    }
+}
